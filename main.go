25ede4bd576a1960e7d9df3cff668dbc13013afc
Michel Barbosa <michel.amb.92@gmail.com>
2024-04-02 09:48:53 -0300

    HTTP server hello world

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
 import "fmt"
+import "net/http"
+import "log"
 
 func main() {
-	fmt.Println("Hello from STC server!")
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello from STC server!")
+	})
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
