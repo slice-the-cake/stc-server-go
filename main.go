192f9093953202c3a5bdc780c37a4e41e0cf4942
Michel Barbosa <michel.amb.92@gmail.com>
2024-04-04 08:23:05 -0300

    Reading the request body in the example endpoint.

--- main.go
+++ main.go
@@ -1,12 +1,20 @@
 package main
 
-import "fmt"
-import "net/http"
-import "log"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
 
 func main() {
 	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello from STC server!")
+		b, err := io.ReadAll(r.Body)
+		if err != nil {
+			fmt.Fprintf(w, "Error while reading body: %s", err)
+			return
+		}
+		fmt.Fprintf(w, "Hello from STC server! Here's the body that was sent: %s", b)
 	})
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
