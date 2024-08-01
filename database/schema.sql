2b1432d940273b98b35593c9282a6c47e910f9bc
Michel Barbosa <michel.amb.92@gmail.com>
2024-07-31 22:50:59 -0300

    Basic user schema.

--- database/schema.sql
+++ database/schema.sql
@@ -1,5 +1,11 @@
--- TODO: WIP
+CREATE TABLE "user" (
+  username VARCHAR(16) NOT NULL,
+  password_hash BYTEA NOT NULL,
+  password_salt BYTEA NOT NULL,
 
-CREATE TABLE user (
-  username VARCHAR(16) NOT NULL
+  -- TODO: consider including (with INCLUDE) password_hash to PK index
+  CONSTRAINT user_pk PRIMARY KEY(username),
+  CONSTRAINT user_username_size_ch CHECK(1 <= length(username)),
+  CONSTRAINT user_password_hash_size_ch CHECK(length(password_hash) = 32),
+  CONSTRAINT user_password_salt_size_ch CHECK(length(password_salt) = 32)
 );
