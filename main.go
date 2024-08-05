f62ee1764fd57ba44aab286d07d47dfbffba7f48
Michel Barbosa <michel.amb.92@gmail.com>
2024-08-05 09:54:42 -0300

    wip: Added `users` endpoint.
    For now it just connects to pg.

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
 	"io"
 	"log"
+	"log/slog"
 	"net/http"
+	"os"
 )
 
 func main() {
@@ -15,6 +20,28 @@ func main() {
 			return
 		}
 		fmt.Fprintf(w, "Hello from STC server! Here's the body that was sent: %s", b)
+	})
+	http.HandleFunc("/users", func(w http.ResponseWriter, r *http.Request) {
+		ctx := context.Background()
+		dbpool, err := pgxpool.New(ctx, os.Getenv("DB_URL"))
+		if err != nil {
+			slog.Error("Error while creating the pg pool: %s", err)
+			w.WriteHeader(500)
+			w.Write([]byte("Database pool error"))
+			return
+		}
+		defer dbpool.Close()
+		var tx pgx.Tx
+		tx, err = dbpool.Begin(ctx)
+		if err != nil {
+			slog.Error("Error while beginning a pg transaction: %s", err)
+			w.WriteHeader(500)
+			w.Write([]byte("Database transaction error"))
+			return
+		}
+		defer tx.Rollback(ctx)
+		defer tx.Conn().Close(ctx)
+		w.Write([]byte("Connected!"))
 	})
 	log.Fatal(http.ListenAndServe(":8090", nil))
 }
